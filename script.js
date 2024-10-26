b40f63d942d0ee2714c8d46c351e3aace3affabe
Carlos Hernandez <hernandez.carlos09@gmail.com>
2024-10-26 13:28:46 -0600

    actions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let contenidoArchivo="";
 function procesarArchivo(){
     const fileInput = document.getElementById("fileInput");
     const optionSelected=document.getElementById("ml").value;
-    const output = document.getElementById("output");
+    generateOptions();
+    
 
     if(fileInput.files.length===0){
         alert("Por favor, seleccione un archivo");
@@ -12,14 +14,14 @@ function procesarArchivo(){
     const reader=new FileReader();
 
     reader.onload = function(e){
-        const contenido=e.target.result;
+        contenidoArchivo=e.target.result;
 
         switch(optionSelected){
             case "uno":
-                optionOne(contenido);
+                optionOne();
                 break;
             case "dos":
-                optionTwo(contenido);
+                optionTwo();
                 break;
             default:
                 alert("Opcion no valida");
@@ -31,17 +33,26 @@ function procesarArchivo(){
 }
 
 
-function optionOne(contenido){
+function optionOne(){
     let fragmento;
     document.getElementById("output").innerHTML+="<h3>Opcion uno</h3><br><i>";
-    fragmento=contenido.slice(0,100);
+    fragmento=contenidoArchivo.slice(0,100);
     document.getElementById("output").innerHTML+=fragmento;
     document.getElementById("output").innerHTML+="</i>";
 }
 
-function optionTwo(contenido){
-    let fragmento= contenido.slice(0,50);
+function optionTwo(){
+    let fragmento= contenidoArchivo.slice(0,50);
     document.getElementById("output").innerHTML+="<h3>Opcion dos</h3><br><b>";
     document.getElementById("output").innerHTML+=fragmento;
     document.getElementById("output").innerHTML+="</p>";
+}
+
+function generateOptions(){
+    const content = document.getElementById("operations");
+    content.innerHTML = `
+        <button onclick="entrenamiento()">Entrenamiento</button>
+        <button onclick="prediccion()">Prediccion</button>
+        <button onclick="graficos()">Graficos</button>
+    `;
 }
